ca12031f93d6e270bb262d3b4315436c9e1bc5a7
CHETAN ANAND <ketan.anand@flipkart.com>
2024-10-18 16:21:27 +0530

    Color sort code

--- src/main/java/SortColor.java
+++ src/main/java/SortColor.java
@@ -4,12 +4,29 @@ public class SortColor {
 
         // Write your code here
         int start = 0, end = colors.length-1, current =0;
-        while(start<end){
+        while(current<=end ){
             if(colors[current]==0){
                 colors[current]=colors[start];
-
+                colors[start]=0;
+                start++;
+                current++;
+                continue;
+            }
+            if(colors[current]==2){
+                colors[current]=colors[end];
+                colors[end]=2;
+               end--;
+            }
+            if(colors[current]==1) {
+                current++;
             }
         }
         return colors;
+    }
+
+    public static void main(String[] args) {
+        int[] num = new int[]{0, 1, 0};
+        sortColors(num);
+
     }
 }
